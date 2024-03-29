9c74f03febf4555ca57360a73579990960109e65
Manjeet <manjeet.helpall@gmail.com>
2024-03-29 12:13:16 +0530

    new add

---
+++ src/com/pack1/Cls2.java
@@ -0,0 +1,14 @@
+package com.pack1;
+
+public class Cls2 {
+	public void m1() {
+		int a = 10;
+		int b = 10;
+
+		System.out.println(a + b);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("create method");
+	}
+}
